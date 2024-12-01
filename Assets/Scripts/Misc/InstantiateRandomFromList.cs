81fcaee9748704cd14b086f9098c37c97196da27
WheeroWeyi <37834956+WheeroWeyi@users.noreply.github.com>
2024-12-01 17:00:47 +0100

    added room decorator script

---
+++ Assets/Scripts/Misc/InstantiateRandomFromList.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateRandomFromList : MonoBehaviour
+{
+    [System.Serializable]
+    public class WeightedPrefab
+    {
+        public GameObject prefab;
+        public float weight;
+    }
+
+    public WeightedPrefab[] prefabs;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        GameObject selectedPrefab = GetRandomPrefab();
+        if (selectedPrefab != null)
+        {
+            Instantiate(selectedPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    GameObject GetRandomPrefab()
+    {
+        float totalWeight = 0f;
+        foreach (var prefab in prefabs)
+        {
+            totalWeight += prefab.weight;
+        }
+
+        float randomValue = Random.Range(0, totalWeight);
+        float cumulativeWeight = 0f;
+
+        foreach (var prefab in prefabs)
+        {
+            cumulativeWeight += prefab.weight;
+            if (randomValue < cumulativeWeight)
+            {
+                return prefab.prefab;
+            }
+        }
+
+        return null;
+    }
+}
