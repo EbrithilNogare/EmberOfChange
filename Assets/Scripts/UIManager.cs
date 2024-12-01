5adeebcc4804cc9c7f236e64e64096ba85a9bdec
Monika Bošániová <monika.bosaniova@gmail.com>
2024-12-01 13:01:23 +0100

    UI FE update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,8 +6,10 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] StatsManager stats;
 
-    [SerializeField] TMP_Text healthText;
-    [SerializeField] TMP_Text fireExtinguisherText;
+    //[SerializeField] TMP_Text healthText;
+    //[SerializeField] TMP_Text fireExtinguisherText;
+    
+    [SerializeField] GameObject FEParent;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,9 @@ public class UIManager : MonoBehaviour
         stats.OnHit.AddListener(UpdateHealth);
         stats.OnDeath.AddListener(ShowDeath);
         stats.OnExtinguisherUpdate.AddListener(UpdateExtinguisher);
-        healthText.text = "Health: " + stats.health;
-        fireExtinguisherText.text = "FE: " + stats.fireExtinguisher;
+        //healthText.text = "Health: " + stats.health;
+        FEParent.transform.GetChild(0).gameObject.SetActive(true);
+        //fireExtinguisherText.text = "FE: " + stats.fireExtinguisher;
     }
 
     // Update is called once per frame
@@ -30,7 +33,7 @@ public class UIManager : MonoBehaviour
     public void UpdateHealth()
     {
         Debug.Log("UI: UpdateHealth");
-        healthText.text = "Health: " + stats.health;
+        //healthText.text = "Health: " + stats.health;
     }
 
     //TODO: UI
@@ -43,7 +46,12 @@ public class UIManager : MonoBehaviour
     //TODO: UI
     public void UpdateExtinguisher(int value)
     {
-        Debug.Log("UI: UpdateExtinguisher");
-        fireExtinguisherText.text = "FE: " + stats.fireExtinguisher;
+        Debug.Log("UI: UpdateExtinguisher " + value);
+        foreach (Transform child in FEParent.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        FEParent.transform.GetChild(value).gameObject.SetActive(true);
+        //fireExtinguisherText.text = "FE: " + stats.fireExtinguisher;
     }
 }
